1f533b4aa49acafc90e09c3e522c7e31c416ecfb
Rajkumar Shakya <rjkumarshakya1985@gmail.com>
2026-01-20 14:32:51 +0530

    change password implementation

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -2,23 +2,36 @@ import { Component } from '@angular/core';
 import { BadgeModule } from 'primeng/badge';
 import { ButtonModule } from 'primeng/button';
 import { MenubarModule } from 'primeng/menubar';
-import { MenuItem } from 'primeng/api';
+import { MenuItem, MessageService } from 'primeng/api';
 import { Menu } from 'primeng/menu';
 import { LocalStorageService } from '../../core/services/local-storage.service';
+import { DialogModule } from 'primeng/dialog';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { UserService } from '../../core/services/user.service';
 
 @Component({
   selector: 'app-header',
   standalone: true, 
-  imports: [MenubarModule, ButtonModule, BadgeModule,Menu],
+  imports: [MenubarModule, ButtonModule, BadgeModule,Menu,DialogModule,
+    ReactiveFormsModule,InputTextModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
+  visible = false;
   items: MenuItem[] | undefined;
   roleName: string = '';
   isLogin: boolean = false;
+  credentialForm!: FormGroup;
 
-  constructor(private storage: LocalStorageService) {
+  constructor(private fb: FormBuilder,private storage: LocalStorageService,
+    private userService:UserService,private messageService: MessageService,) {
+
+    this.credentialForm = this.fb.group({
+      oldPassword: ['', Validators.required],
+      newPassword: ['',Validators.required]
+    });
 
     this.initializeMenu();
      this.loadHeaderData();
@@ -60,9 +73,36 @@ export class Header {
   }
    showDailog():void
    {
-
+       this.visible = true;
+       this.credentialForm.reset();
    }
-  toggleSidebar() {
+
+   toggleSidebar() {
     // emit an event if you want to control sidebar visibility from parent
-  }
+   }
+
+   changePassword():void
+    {
+       this.userService.changePassword(this.credentialForm.value).subscribe({
+            next: (status:boolean) => {
+               
+              this.messageService.add({
+                    severity: 'success',
+                    summary: 'Password Changed',
+                    detail: 'Your password has been changed successfully. You will be logged out.',
+                life: 3000
+               });
+
+     
+               this.visible = false;
+               this.credentialForm.reset();
+
+      
+                 setTimeout(() => {
+                  this.logout();
+                 }, 3000);
+
+            }
+      });
+    }
 }
