84d99e8275484c126449b0953674935eb3ce3193
ash0955 <ashishyadavmontu@gmail.com>
2026-08-20 17:10:27 +0530

    Sticker Default

--- src/app/layouts/supplier-layout/pages/sticker-size-setting/sticker-size-setting.ts
+++ src/app/layouts/supplier-layout/pages/sticker-size-setting/sticker-size-setting.ts
@@ -181,8 +181,7 @@ fields(sticker: StickerPrint): StickerPrintFieldSetting[] {
         return sticker.supplierCode;
       case 'companyShortName':
         return sticker.stickerSetting?.companyShortName ?? 'DEMO';
-        return sticker.stickerSetting?.companyShortName ?? 'DEMO';
-         case 'wholeSaleRate':
+          case 'wholeSaleRate':
         return sticker.wholeSaleRate;
       case 'productName':
         return sticker.productName;
